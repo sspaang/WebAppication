609ddbc3c9d39953b376957bc4413e4ecab35689
Jacha <ja_nun_@hotmail.com>
2020-10-09 18:57:29 +0700

    header to login page instead

--- register_db.php
+++ register_db.php
@@ -16,6 +16,7 @@
         if ($password_1 != $password_2) {
             array_push($errors, "The password do not match");
             $_SESSION['error'] = "The password do not match";
+        }
 
             $user_check_query = "SELECT * FROM users WHERE username = '$username' OR email = '$email' LIMIT 1";
             $query = mysqli_query($conn, $user_check_query);
@@ -24,14 +25,16 @@
             if ($result) { // if user exisis
                 if ($result['username'] === $username) {
                     array_push($errors, "Username already exists");
+                    $_SESSION['error'] = "Username already exists";
                 }
                 if ($result['email'] === $email) {
                     array_push($errors, "Email already exists");
+                    $_SESSION['error'] = "Email already exists";
                 }
             }
 
             if (count($errors) == 0) {
-                //$password = $password_1;
+                $password = $password_1;
 
                 $sql = "INSERT INTO users (username, position,email, password, firstname, lastname) VALUES ('$username', '$position','$email', '$password', '$firstname', '$lastname')";
                 mysqli_query($conn, $sql);
@@ -39,14 +42,10 @@
                 $_SESSION['username'] = $username; // เก็บ session username
                 $_SESSION['position'] = $position;
 
-                if ($position == '1') {
-                    header('location: student_home.php');
-                } elseif ($position == '2') {
-                    header('location: teacher_home.php');
-                } 
+                header('location: login.php');
+
             } else {
                 header("location: register.php");
             }
-        }
-    }
+       }
 ?>
